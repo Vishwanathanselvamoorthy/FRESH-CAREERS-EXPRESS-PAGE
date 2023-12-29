db661128e5464053fd900bb8b66e3c5062d2858c
VISHWA <147639866+Vishwanathanselvamoorthy@users.noreply.github.com>
2023-12-29 06:32:48 +0000

    added express app , index.html,style.css

---
+++ src/app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+
+const path = require('path');
+
+const app = express();
+
+app.use(express.static(path.join(__dirname,'public')));
+
+app.get('/',(req,res)=>{
+
+    //res.send("Hello World");
+
+    res.sendFile(path.join(__dirname,'pages/index.html'))
+    
+
+})
+
+const port = 5000;
+
+app.listen(port,()=>{
+
+    console.log(`SERVER CLICK HERE  ➡️  https://localhost:${port}`);
+
+})
+
